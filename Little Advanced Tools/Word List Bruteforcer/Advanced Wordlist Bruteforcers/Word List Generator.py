dbd17bfcf99f0d16ce11405e32c1fd1e5562ed6f
Cherry6628 <162563824+Cherry6628@users.noreply.github.com>
2025-04-01 12:08:58 +0530

    Update Word List Generator.py

--- Little Advanced Tools/Word List Bruteforcer/Advanced Wordlist Bruteforcers/Word List Generator.py
+++ Little Advanced Tools/Word List Bruteforcer/Advanced Wordlist Bruteforcers/Word List Generator.py
@@ -1,8 +1,8 @@
 try:
     min_length = int(input("Min Length >> "))
     max_length = int(input("Max Length >> "))
-    characters = input("Characters >> ").split()
-    # a b c d e f g h   i j k l m n o p q r s t u v w x y z  A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 0 1 2 3 4 5 6 7 8 9
+    characters = input("Characters >> ").split(" ")
+    # a b c d e f g h   i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 0 1 2 3 4 5 6 7 8 9
 except:
     exit()
 
